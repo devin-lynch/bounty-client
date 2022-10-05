496b6854104812a7c1f7c4bccf0d70eee4b15ed7
devin-lynch <devin.l.lynch@gmail.com>
2022-10-05 13:31:07 -0500

    Set up nav

--- src/components/partials/NavBar.jsx
+++ src/components/partials/NavBar.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom'
+
 export default function Navbar() {
     return (
-        <div>
-            NavBar Component
-        </div>
+        <nav>
+            <ul style={{ listStyleType: 'none' }}>
+                <li>
+                    <Link to='/home'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/bounties'>All Bounties</Link>    
+                </li>
+                <li>
+                    <Link to='/bounties/new'>Create Bounty</Link>    
+                </li>
+            </ul>
+        </nav>
     )
 }
