a9faa0d08d1923395b10e5d731f0ac5f2531d9c8
devin-lynch <devin.l.lynch@gmail.com>
2022-10-05 13:09:28 -0500

    Import jsx routes to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavBar from './components/partials/NavBar'
+import Home from './components/routes/Home';
+import Bounties from './components/routes/Bounties';
+import Bounty from './components/routes/Bounty';
+import EditBounty from './components/routes/EditBounty';
+import NewBounty from './components/routes/NewBounty';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
